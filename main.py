0c0e0d3a707213816dad9127174f9a86aae879ff
Jisoo2073 <hwang332@purdue.edu>
2026-08-04 18:37:26 +0300

    Create basic music player layout

--- main.py
+++ main.py
@@ -21,12 +21,67 @@ from PyQt5.QtWidgets import (
 class MusicPlayer(QWidget):
     def __init__(self):
         super().__init__()
+
+        # Widgets
+        self.play_button = QPushButton("Play",self) #self set the parent class
+        self.pause_button = QPushButton("Pause",self)
+        self.next_button = QPushButton("Next", self)
+        self.previous_button = QPushButton("Previous", self)
+        self.add_music_button = QPushButton("Add music", self)
+
+        self.positionSlider = QSlider(Qt.Horizontal, self) #Slider is horizontal
+
+        self.playlist = QListWidget(self) # Playlist list
+
+        self.album_cover = QLabel(self)
+        self.album_cover.setFixedSize(250,250)
         
         self.init_ui()
 
+
     def init_ui(self):
         self.setWindowTitle("Py Music Player (v0.1)")
-        self.resize(500,500)
+        self.resize(700,500)
+
+        main_layout = QHBoxLayout()  # playlist left / control pad right
+
+        # Playlist & Add Music Part
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.add_music_button)
+        left_layout.addWidget(self.playlist)
+
+        # ControlPad Part
+        right_layout = QVBoxLayout()
+        right_layout.addWidget(self.album_cover)
+        right_layout.addWidget(self.positionSlider)
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.previous_button)
+        button_layout.addWidget(self.play_button)
+        button_layout.addWidget(self.pause_button)
+        button_layout.addWidget(self.next_button)
+
+        right_layout.addLayout(button_layout)
+
+        # Merge two sections into Main Layout
+        main_layout.addLayout(left_layout)
+        main_layout.addLayout(right_layout)
+
+
+        # Styles
+
+        # Set Text
+        self.album_cover.setText("Album Cover")
+
+        # Alignment
+        self.album_cover.setAlignment(Qt.AlignCenter)
+        self.album_cover.setStyleSheet("""
+            border: 2px solid gray;
+        """)
+        
+
+        self.setLayout(main_layout)
+
 
 # How does the window works
 if __name__ == "__main__":
